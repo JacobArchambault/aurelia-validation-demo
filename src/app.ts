94bea8dd3c8a8892f05763d7e5e328e7bd5609e3
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-08-31 11:46:22 -0400

    Remove unused imports

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import { autoinject } from 'aurelia-framework';
-import { TextHandler } from 'aurelia-loader-nodejs';
 import { ValidationController, ValidationRules } from "aurelia-validation";
-import { isNumberObject } from "util/types";
 
 @autoinject
 export class App {
