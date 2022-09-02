b00d0e96548709b9893a740c2078d7834ad83f00
Jacob Archambault <Jacob.Archambault@venminder.com>
2022-09-02 15:13:11 -0400

    Add example of validating a select dropdown

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import { inject, NewInstance } from 'aurelia-framework';
 import { ValidationController, ValidationRules } from "aurelia-validation";
-
+export interface IProduct {
+  id: number;
+  name: string;
+}
 @inject(NewInstance.of(ValidationController))
 export class App {
   private name: string;
   private address: string;
   private age: number;
   private phoneNumber: string;
+  products: IProduct[] = [
+    { id: 0, name: 'Motherboard' },
+    { id: 1, name: 'CPU' },
+    { id: 2, name: 'Memory' },
+  ];
 
+  selectedProductId: number = null;
   private message: string;
   constructor(private readonly controller: ValidationController) {
     ValidationRules
@@ -22,6 +31,8 @@ export class App {
     .displayName("Phone number")
     .required()
     .matches(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/)
+    .ensure((m: App) => m.selectedProductId)
+    .required()
     .on(this);
   }
 
